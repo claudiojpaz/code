230efa80752e50bb50ec42674591f9fb3341a7a2
Claudio J. Paz <claudiojpaz@gmail.com>
2021-04-12 10:20:59 -0300

    Agrega suma con variables

---
+++ u3/u3-conversion-op-suma-2.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+// u3-conversion-op-suma.c
+
+int main (void)
+{
+  int n1, n2, suma;
+
+  n1 = 7;
+  n2 = 8;
+  suma = n1 + n2;
+
+  printf("%d+%d=%d\n", n1, n2, suma);
+
+  return 0;
+}
